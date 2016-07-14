461612153f1759e607eb7a383237f3b35818cc3a
Juan Jose Meroño Sánchez <jjmerono@um.es>
2016-07-14 15:14:03 +0200

    More complex test

--- src/test/scala/computerdatabase/SakaiSimulation.scala
+++ src/test/scala/computerdatabase/SakaiSimulation.scala
@@ -8,9 +8,11 @@ import io.gatling.jdbc.Predef._
 class SakaiSimulation extends Simulation {
 
 	val successStatus: Int = 200
+	val pauseMin: Int = 1
+	val pauseMax: Int = 1
 	val httpProtocol = http
 		.baseURL("https://trunk-mysql.nightly.sakaiproject.org")
-		.inferHtmlResources(BlackList(""".*(\.css|\.js|\.png|\.jpg).*"""), WhiteList())
+		.inferHtmlResources(BlackList(""".*(\.css|\.js|\.png|\.jpg|\.gif|thumb).*"""), WhiteList())
 		.userAgentHeader("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36")
 
 	val headers = Map(
@@ -27,49 +29,62 @@ class SakaiSimulation extends Simulation {
 				.get("/portal")
 				.headers(headers)
 				.check(status.is(successStatus)))
-			.pause(2)
+			.pause(pauseMin,pauseMax)
 			.exec(http("XLogin")
 				.post("/portal/xlogin")
 				.headers(headers)
 				.formParam("eid", "instructor")
 				.formParam("pw", "sakai")
 				.formParam("submit", "Log+In")
-				.check(status.is(successStatus)))
-			.pause(2)
+				.check(status.is(successStatus))
+				.check(css("div.fav-title > a","href").findAll.saveAs("siteUrls"))
+				.check(css("div.fav-title > a","title").findAll.saveAs("siteTitles")))
+			.pause(pauseMin,pauseMax)
+			.exec(session => { 
+				val mySites: Vector[(String,String)] = (session("siteTitles").as[Vector[String]] zip session("siteUrls").as[Vector[String]])
+				session.set("sites", mySites)
+			})
 		} 
 	}
 
 	object GoSite {
-		val go = group("Site") {
-			exec(http("GetSites")
-				.get("/portal")
-				.headers(headers)
-				.check(status.is(successStatus))
-				.check(css("div.fav-title > a","href").findAll.saveAs("siteUrls")))
-			.pause(1)
-			.exec(http("FavSites")
+		val go = group("Sites") {
+			/**.exec(http("FavSites")
 				.get("/portal/favorites/list")
 				.headers(headers)
 				.check(status.is(successStatus)))
-			.pause(1)
-			.foreach("${siteUrls}","url") {
-				exec(http("GoToSite ${url}")
-					.get("${url}")
-					.headers(headers)
-					.check(status.is(successStatus)))
-				.pause(2)
+			.pause(pauseMin,pauseMax)
+			.exec(session => {
+				print(session)
+				session 
+			})*/
+			foreach("${sites}","site") {
+				group("${site._1}") {
+					exec(http("/site/${site._1}")
+						.get("${site._2}")
+						.headers(headers)
+						.check(status.is(successStatus))
+						.check(css("title:contains('Sakai : ${site._1} :')").exists)
+						.check(css("a.Mrphs-toolsNav__menuitem--link","href").findAll.saveAs("toolUrls"))
+						.check(css("span.Mrphs-toolsNav__menuitem--title").findAll.saveAs("toolNames")))
+					.pause(pauseMin,pauseMax)
+					.exec(session => { 
+						val myTools: Vector[(String,String)] = (session("toolNames").as[Vector[String]] zip session("toolUrls").as[Vector[String]])
+						session.set("tools", myTools)
+					})
+					.foreach("${tools}","tool") {
+						doIf(session => !session("tool").as[(String,String)]._2.contains("/portal/help/main")) {
+							exec(http("/tool/${tool._1}")
+								.get("${tool._2}")
+								.headers(headers)
+								.check(status.is(successStatus))
+								.check(css("title").is("Sakai : ${site._1} : ${tool._1}")))
+							.pause(pauseMin,pauseMax)
+						}
+					}
+				}
 			}
 		} 
-	}
-
-	object GoTool {
-		val go = group("Tool") {
-			exec(http("GoToTool")
-				.get("/portal/site/c20d9953-c71f-4989-be2a-21e67d937151/tool/d5dcaa3b-561e-4823-b732-95ac97a86c59")
-				.headers(headers)
-				.check(status.is(successStatus)))
-			.pause(3)
-		}
 	}
 
 	object Logout {
@@ -81,7 +96,7 @@ class SakaiSimulation extends Simulation {
 		}
 	}
 
-	val scn = scenario("SakaiSimulation").exec(Login.login,GoSite.go,GoTool.go,Logout.logout)
+	val scn = scenario("SakaiSimulation").exec(Login.login,GoSite.go,Logout.logout)
 
 	setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
 }
