1a4892fc10b1c52c97cbeea6d0becac241cbe77f
Juan Jose Meroño Sánchez <jjmerono@um.es>
2016-07-15 14:20:15 +0200

    25 users test

--- src/test/scala/computerdatabase/SakaiSimulation.scala
+++ src/test/scala/computerdatabase/SakaiSimulation.scala
@@ -4,14 +4,15 @@ import scala.concurrent.duration._
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import io.gatling.jdbc.Predef._
+import java.net._
 
 class SakaiSimulation extends Simulation {
 
 	val successStatus: Int = 200
-	val pauseMin: Int = 1
-	val pauseMax: Int = 1
+	val pauseMin: Int = Integer.getInteger("min-pause",1)
+	val pauseMax: Int = Integer.getInteger("max-pause",1)
 	val httpProtocol = http
-		.baseURL("https://trunk-mysql.nightly.sakaiproject.org")
+		.baseURL(System.getProperty("test-url"))
 		.inferHtmlResources(BlackList(""".*(\.css|\.js|\.png|\.jpg|\.gif|thumb).*"""), WhiteList())
 		.userAgentHeader("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36")
 
@@ -23,6 +24,8 @@ class SakaiSimulation extends Simulation {
 		"Connection" -> "keep-alive",
 		"Upgrade-Insecure-Requests" -> "1")
 
+	val users = csv("user_credentials.csv").random
+
 	object Login {
 		val login = group("Login") {
 			exec(http("Portal")
@@ -30,11 +33,12 @@ class SakaiSimulation extends Simulation {
 				.headers(headers)
 				.check(status.is(successStatus)))
 			.pause(pauseMin,pauseMax)
-			.exec(http("XLogin")
+			.feed(users)
+			.exec(http("XLogin [${username}]")
 				.post("/portal/xlogin")
 				.headers(headers)
-				.formParam("eid", "instructor")
-				.formParam("pw", "sakai")
+				.formParam("eid", "${username}")
+				.formParam("pw", "${password}")
 				.formParam("submit", "Log+In")
 				.check(status.is(successStatus))
 				.check(css("div.fav-title > a","href").findAll.saveAs("siteUrls"))
@@ -49,15 +53,6 @@ class SakaiSimulation extends Simulation {
 
 	object GoSite {
 		val go = group("Sites") {
-			/**.exec(http("FavSites")
-				.get("/portal/favorites/list")
-				.headers(headers)
-				.check(status.is(successStatus)))
-			.pause(pauseMin,pauseMax)
-			.exec(session => {
-				print(session)
-				session 
-			})*/
 			foreach("${sites}","site") {
 				group("${site._1}") {
 					exec(http("/site/${site._1}")
@@ -69,17 +64,34 @@ class SakaiSimulation extends Simulation {
 						.check(css("span.Mrphs-toolsNav__menuitem--title").findAll.saveAs("toolNames")))
 					.pause(pauseMin,pauseMax)
 					.exec(session => { 
-						val myTools: Vector[(String,String)] = (session("toolNames").as[Vector[String]] zip session("toolUrls").as[Vector[String]])
+						val myTools: Vector[(String,String)] = (session("toolNames").as[Vector[String]] zip session("toolUrls").as[Vector[String]].map(s => URLDecoder.decode(s,"UTF-8")))
 						session.set("tools", myTools)
 					})
 					.foreach("${tools}","tool") {
+						/** Do not process help tool */
 						doIf(session => !session("tool").as[(String,String)]._2.contains("/portal/help/main")) {
 							exec(http("/tool/${tool._1}")
 								.get("${tool._2}")
 								.headers(headers)
 								.check(status.is(successStatus))
-								.check(css("title").is("Sakai : ${site._1} : ${tool._1}")))
+								.check(css("title").is("Sakai : ${site._1} : ${tool._1}"))
+								.check(css("iframe","src").findAll.optional.saveAs("frameUrls"))
+								.check(css("iframe","title").findAll.optional.saveAs("frameNames")))
 							.pause(pauseMin,pauseMax)
+							/** Take care of iframed tools */
+							.doIf("${frameUrls.exists()}") {
+								exec(session => { 
+									val myFrames: Vector[(String,String)] = (session("frameNames").as[Vector[String]] zip session("frameUrls").as[Vector[String]].map(s => URLDecoder.decode(s,"UTF-8")))
+									session.set("frames", myFrames)
+								})
+								.foreach("${frames}","frame") {
+									exec(http("/frame/${frame._1}")
+										.get("${frame._2}")
+										.headers(headers)
+										.check(status.is(successStatus)))
+									.pause(pauseMin,pauseMax)
+								}
+							}
 						}
 					}
 				}
@@ -98,5 +110,5 @@ class SakaiSimulation extends Simulation {
 
 	val scn = scenario("SakaiSimulation").exec(Login.login,GoSite.go,Logout.logout)
 
-	setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
+	setUp(scn.inject(atOnceUsers(25))).protocols(httpProtocol)
 }
