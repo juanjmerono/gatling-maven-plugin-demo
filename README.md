beec9ef25bba28e9ee052a408646df3c90e2a4c2
Juan Jose Meroño Sánchez <jjmerono@um.es>
2016-07-19 10:30:11 +0200

    More changes

--- README.md
+++ README.md
@@ -1,12 +1,61 @@
-gatling-maven-plugin-demo
+Sakai Gatling Stress Test
 =========================
 
-Simple showcase of a maven project using the gatling-maven-plugin.
+This is a Simple Stress Test for Sakai 11+ instances.
 
 To test it out, simply execute the following command:
 
-    $mvn gatling:execute -Dgatling.simulationClass=computerdatabase.BasicSimulation
-
-or simply:
-
     $mvn gatling:execute
+
+Each time you run this you'll get a result in target/sakaisimulation-xxxxxxx folder.
+
+Stress Test Use Case
+====================
+
+This test is really simple, I hope we can add more complex cases soon.
+
+Use two different type of users "random" and "exahustive".
+
+The random users follow these steps:
+	- Go to the portal
+	- Login
+	- Go to one random site
+	- Got to one random tool
+	- Logout 
+
+The exahustive users follow these steps:
+	- Go to the portal
+	- Login
+	- Go to each site
+	- Got to each tool on each site
+	- Logout 
+
+Both types of users are running at the same time, so during the test you've got the same number of users of both types.
+
+Setting Up Your Test
+====================
+
+There are several things you can tune in this test:
+
+- Target URL: You can change the URL for the test by typing
+
+	$mvn gatling:execute -Dtesturl=https://my-sakai-instance
+	
+- Concurrent Users and RampUp time: You can change the number of concurrent users and the time to rampup them by typing
+
+	$mvn gatling:execute -DtargetUsers=<Users> -DrampUpTime=<Seconds>
+	
+- Loops: You are able to repeat the test in different ways
+	
+	$mvn gatling:execute -DuserLoop=U -DsiteLoop=S -DtoolLoop=T
+
+	* userLoop: The test case is repeated N times (all the steps)
+	* siteLoop: For random users pick S random sites
+	* toolLoop: For random users pick T tools on each random site
+	
+Exploring the results
+=====================
+
+Gatling return an HTML report that gives you lot of information about the stress test.
+
+Please go to http://gatling.io/ to know more about it.
--- src/test/scala/computerdatabase/SakaiSimulation.scala
+++ src/test/scala/computerdatabase/SakaiSimulation.scala
@@ -11,9 +11,15 @@ class SakaiSimulation extends Simulation {
 	val successStatus: Int = 200
 	val pauseMin: Int = Integer.getInteger("min-pause",1)
 	val pauseMax: Int = Integer.getInteger("max-pause",1)
+	val targetUsers: Int = Integer.getInteger("target-users",1)
+	val rampUpTime: Int = Integer.getInteger("rampup-time",10)
+	val siteLoop: Int = Integer.getInteger("site-loop",1)
+	val toolLoop: Int = Integer.getInteger("tool-loop",1)
+	val userLoop: Int = Integer.getInteger("user-loop",1)
+	
 	val httpProtocol = http
 		.baseURL(System.getProperty("test-url"))
-		.inferHtmlResources(BlackList(""".*(\.css|\.js|\.png|\.jpg|\.gif|thumb).*"""), WhiteList())
+		/**.inferHtmlResources(BlackList(".*(\.css|\.js|\.png|\.jpg|\.gif|thumb).*"), WhiteList())*/
 		.userAgentHeader("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36")
 
 	val headers = Map(
@@ -26,15 +32,20 @@ class SakaiSimulation extends Simulation {
 
 	val users = csv("user_credentials.csv").random
 
-	object Login {
-		val login = group("Login") {
+	object Gateway {
+		val gateway = group("Gateway") {
 			exec(http("Portal")
 				.get("/portal")
 				.headers(headers)
 				.check(status.is(successStatus)))
 			.pause(pauseMin,pauseMax)
-			.feed(users)
-			.exec(http("XLogin [${username}]")
+		}
+	}
+
+	object Login {
+		val login = group("Login") {
+			feed(users)
+			.exec(http("XLogin")
 				.post("/portal/xlogin")
 				.headers(headers)
 				.formParam("eid", "${username}")
@@ -46,57 +57,99 @@ class SakaiSimulation extends Simulation {
 			.pause(pauseMin,pauseMax)
 			.exec(session => { 
 				val mySites: Vector[(String,String)] = (session("siteTitles").as[Vector[String]] zip session("siteUrls").as[Vector[String]])
-				session.set("sites", mySites)
+				session.set("sites", util.Random.shuffle(mySites))
 			})
 		} 
 	}
 
-	object GoSite {
-		val go = group("Sites") {
-			foreach("${sites}","site") {
-				group("${site._1}") {
-					exec(http("/site/${site._1}")
-						.get("${site._2}")
+	object ExploreTool {
+		val explore = /** Do not process help tool */
+			doIf(session => !session("tool").as[(String,String)]._2.contains("/portal/help/main")) {
+				group("${tool._1}") {
+					exec(http("${tool._1}")
+						.get("${tool._2}")
 						.headers(headers)
 						.check(status.is(successStatus))
-						.check(css("title:contains('Sakai : ${site._1} :')").exists)
-						.check(css("a.Mrphs-toolsNav__menuitem--link","href").findAll.saveAs("toolUrls"))
-						.check(css("span.Mrphs-toolsNav__menuitem--title").findAll.saveAs("toolNames")))
+						.check(css("title").is("Sakai : ${site._1} : ${tool._1}"))
+						.check(css("iframe","src").findAll.optional.saveAs("frameUrls"))
+						.check(css("iframe","title").findAll.optional.saveAs("frameNames")))
 					.pause(pauseMin,pauseMax)
-					.exec(session => { 
-						val myTools: Vector[(String,String)] = (session("toolNames").as[Vector[String]] zip session("toolUrls").as[Vector[String]].map(s => URLDecoder.decode(s,"UTF-8")))
-						session.set("tools", myTools)
-					})
-					.foreach("${tools}","tool") {
-						/** Do not process help tool */
-						doIf(session => !session("tool").as[(String,String)]._2.contains("/portal/help/main")) {
-							exec(http("/tool/${tool._1}")
-								.get("${tool._2}")
+					/** Take care of all iframed tools */
+					.doIf("${frameUrls.exists()}") {
+						exec(session => { 
+							val myFrames: Vector[(String,String)] = (session("frameNames").as[Vector[String]] zip session("frameUrls").as[Vector[String]].map(s => URLDecoder.decode(s,"UTF-8")))
+							session.set("frames", util.Random.shuffle(myFrames))
+						})
+						.foreach("${frames}","frame") {
+							exec(http("${frame._1}")
+								.get("${frame._2}")
 								.headers(headers)
-								.check(status.is(successStatus))
-								.check(css("title").is("Sakai : ${site._1} : ${tool._1}"))
-								.check(css("iframe","src").findAll.optional.saveAs("frameUrls"))
-								.check(css("iframe","title").findAll.optional.saveAs("frameNames")))
+								.check(status.is(successStatus)))
 							.pause(pauseMin,pauseMax)
-							/** Take care of iframed tools */
-							.doIf("${frameUrls.exists()}") {
-								exec(session => { 
-									val myFrames: Vector[(String,String)] = (session("frameNames").as[Vector[String]] zip session("frameUrls").as[Vector[String]].map(s => URLDecoder.decode(s,"UTF-8")))
-									session.set("frames", myFrames)
-								})
-								.foreach("${frames}","frame") {
-									exec(http("/frame/${frame._1}")
-										.get("${frame._2}")
-										.headers(headers)
-										.check(status.is(successStatus)))
-									.pause(pauseMin,pauseMax)
-								}
-							}
 						}
 					}
 				}
 			}
-		} 
+		
+	}
+
+	object BrowseTools {
+		val browse = (random: Boolean) =>
+			group("Tools") {
+				doIfOrElse(random) {
+					repeat(toolLoop) {
+						exec(session => { 
+							session.set("tool",session("tools").as[Vector[String]].lift(util.Random.nextInt(session("tools").as[Vector[String]].size)).get)
+						})
+						.exec(ExploreTool.explore)
+					}
+					 
+				}
+				{
+					foreach("${tools}","tool") {
+						ExploreTool.explore
+					}
+				}
+			}
+	}
+
+	object ExploreSite {
+		val explore = (random: Boolean) => exec(
+			group("GetSite") {
+				exec(http("GetSite")
+					.get("${site._2}")
+					.headers(headers)
+					.check(status.is(successStatus))
+					.check(css("title:contains('Sakai : ${site._1} :')").exists)
+					.check(css("a.Mrphs-toolsNav__menuitem--link","href").findAll.saveAs("toolUrls"))
+					.check(css("span.Mrphs-toolsNav__menuitem--title").findAll.saveAs("toolNames")))
+				.pause(pauseMin,pauseMax)
+				.exec(session => { 
+					val myTools: Vector[(String,String)] = (session("toolNames").as[Vector[String]] zip session("toolUrls").as[Vector[String]].map(s => URLDecoder.decode(s,"UTF-8")))
+					session.set("tools", util.Random.shuffle(myTools))
+				})
+			},
+			BrowseTools.browse(random)
+		)
+	}
+
+	object BrowseSites {
+		val browse = (random: Boolean) =>
+			group("Sites") {
+				doIfOrElse(random) {
+					repeat(siteLoop) {
+						exec(session => { 
+							session.set("site",session("sites").as[Vector[String]].lift(util.Random.nextInt(session("sites").as[Vector[String]].size)).get)
+						})
+						.exec(ExploreSite.explore(random))
+					}
+				}
+				{
+					foreach("${sites}","site") {
+						ExploreSite.explore(random)
+					}
+				}
+			}
 	}
 
 	object Logout {
@@ -107,8 +160,25 @@ class SakaiSimulation extends Simulation {
 				.check(status.is(successStatus)))
 		}
 	}
+	
+	object SakaiSimulation {
+		val test = (random: Boolean) => repeat(userLoop) {
+			exec(Gateway.gateway,Login.login,BrowseSites.browse(random),Logout.logout)
+		}
+	}
+	
 
-	val scn = scenario("SakaiSimulation").exec(Login.login,GoSite.go,Logout.logout)
+	val randomUsersScn = scenario("SakaiRandomUserSimulation").exec(SakaiSimulation.test(true))
+	val exhaustiveUsersScn = scenario("SakaiExhaustiveUserSimulation").exec(SakaiSimulation.test(false))
 
-	setUp(scn.inject(atOnceUsers(25))).protocols(httpProtocol)
+	setUp(
+		randomUsersScn.inject(
+		    rampUsers(targetUsers) over (rampUpTime seconds)
+		    /** More options here http://gatling.io/docs/2.2.2/general/simulation_setup.html */
+		),
+		exhaustiveUsersScn.inject(
+		    rampUsers(targetUsers) over (rampUpTime seconds)
+		    /** More options here http://gatling.io/docs/2.2.2/general/simulation_setup.html */
+		)
+	).protocols(httpProtocol)
 }
